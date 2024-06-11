be05a6a30b0a6109fa22ff2973ae257f339296ca
Jean-Pierre Portier <jean-pierre.portier@mailgun.com>
2024-06-11 13:09:51 +0200

    feature: highlight SmsRegion usage from client configuration

--- tutorials/sms/auto-subscribe-app/src/main/java/com/mycompany/app/Config.java
+++ tutorials/sms/auto-subscribe-app/src/main/java/com/mycompany/app/Config.java
@@ -3,6 +3,7 @@ package com.mycompany.app;
 import com.sinch.sdk.SinchClient;
 import com.sinch.sdk.domains.sms.SMSService;
 import com.sinch.sdk.models.Configuration;
+import com.sinch.sdk.models.SMSRegion;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 
@@ -26,6 +27,7 @@ public class Config {
                         .setProjectId(projectId)
                         .setKeyId(keyId)
                         .setKeySecret(keySecret)
+                        .setSmsRegion(SMSRegion.US)
                         .build();
 
         return new SinchClient(configuration).sms();
