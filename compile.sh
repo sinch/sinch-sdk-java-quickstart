c9d15142ac37c2404428e3184b24c590f93ef4f2
Jean-Pierre Portier <141755467+JPPortier@users.noreply.github.com>
2024-06-28 17:25:03 +0200

    Adding SMS getting started: user verification using SMS PIN (#8)
    * feature: Adding 'Verify a user using SMS PIN' getting started

--- compile.sh
+++ compile.sh
@@ -4,4 +4,5 @@ mvn -f pom-ci.xml clean spotless:apply
 
 (cd templates/client && mvn clean package)
 (cd tutorials && ./compile.sh)
+(cd getting-started && ./compile.sh)
 
---
+++ getting-started/compile.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+(cd verification/user-verification-using-sms-pin/client && mvn clean package)
