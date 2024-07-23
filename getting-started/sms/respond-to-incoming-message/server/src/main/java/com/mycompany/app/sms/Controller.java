2d3c28311e8fccdcfbfce2b72152564cccc9bfa1
Alex Sberna <alex.sberna@sinch.com>
2024-07-23 15:26:18 -0400

    responding to sms message gs

---
+++ getting-started/sms/respond-to-incoming-message/server/src/main/java/com/mycompany/app/sms/Controller.java
@@ -0,0 +1,50 @@
+package com.mycompany.app.sms;
+
+import com.sinch.sdk.SinchClient;
+import com.sinch.sdk.domains.sms.BatchesService;
+import com.sinch.sdk.domains.sms.WebHooksService;
+import com.sinch.sdk.domains.sms.adapters.SMSService;
+import com.sinch.sdk.domains.sms.models.DeliveryReportBatch;
+import com.sinch.sdk.domains.sms.models.DeliveryReportRecipient;
+import com.sinch.sdk.domains.sms.models.InboundBinary;
+import com.sinch.sdk.domains.sms.models.InboundText;
+import com.sinch.sdk.domains.sms.models.webhooks.WebhooksEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("SMS")
+public class Controller {
+
+  private final SinchClient sinchClient;
+  private final ServerBusinessLogic webhooksBusinessLogic;
+
+  @Autowired
+  public Controller(SinchClient sinchClient, ServerBusinessLogic webhooksBusinessLogic) {
+    this.sinchClient = sinchClient;
+    this.webhooksBusinessLogic = webhooksBusinessLogic;
+  }
+
+  @PostMapping(
+      value = "/SmsEvent",
+      consumes = MediaType.APPLICATION_JSON_VALUE,
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Void> smsDeliveryEvent(@RequestBody String body) {
+
+    WebHooksService webhooks = sinchClient.sms().webHooks();
+
+    // decode the request payload
+    WebhooksEvent event = webhooks.parse(body);
+
+    // let business layer process the request
+    switch (event) {
+      case InboundText e -> webhooksBusinessLogic.processInboundEvent(e);
+      default -> throw new IllegalStateException("Unexpected value: " + event);
+    }
+
+    return ResponseEntity.ok().build();
+  }
+}
---
+++ getting-started/sms/respond-to-incoming-message/server/src/main/java/com/mycompany/app/sms/ServerBusinessLogic.java
@@ -0,0 +1,37 @@
+package com.mycompany.app.sms;
+
+import com.sinch.sdk.SinchClient;
+import com.sinch.sdk.domains.sms.BatchesService;
+import com.sinch.sdk.domains.sms.models.InboundText;
+import com.sinch.sdk.domains.sms.models.requests.SendSmsBatchTextRequest;
+import com.sinch.sdk.domains.sms.models.webhooks.WebhooksEvent;
+
+import java.util.Collections;
+import java.util.logging.Logger;
+import org.springframework.stereotype.Component;
+
+@Component("SMSServerBusinessLogic")
+public class ServerBusinessLogic {
+  
+  private final BatchesService batches; 
+  
+  public ServerBusinessLogic(SinchClient sinchClient, BatchesService batchesService){
+    this.batches = sinchClient.sms().batches();
+  }
+
+  private static final Logger LOGGER = Logger.getLogger(ServerBusinessLogic.class.getName());
+
+  public void processInboundEvent(InboundText event) {
+    trace(event);
+
+    batches.send(SendSmsBatchTextRequest.builder()
+        .setTo(Collections.singletonList(event.getFrom()))
+        .setBody("You sent: " + event.getBody())
+        .setFrom(event.getTo())
+        .build());
+      }
+
+  private void trace(WebhooksEvent event) {
+    LOGGER.info("Handle event: " + event);
+  }
+}
