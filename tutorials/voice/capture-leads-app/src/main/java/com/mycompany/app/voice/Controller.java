6e3fd6a3a6b9486a150ac8eef85f18e6f70bed2f
Jean-Pierre Portier <jean-pierre.portier@mailgun.com>
2024-07-24 08:13:40 +0200

    feature (Voice/tutorial): Add authentication validation

--- tutorials/voice/capture-leads-app/src/main/java/com/mycompany/app/voice/Controller.java
+++ tutorials/voice/capture-leads-app/src/main/java/com/mycompany/app/voice/Controller.java
@@ -1,26 +1,29 @@
 package com.mycompany.app.voice;
 
 import com.sinch.sdk.domains.voice.VoiceService;
+import com.sinch.sdk.domains.voice.WebHooksService;
 import com.sinch.sdk.domains.voice.models.webhooks.CallEvent;
 import java.util.Map;
 import java.util.Objects;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController("Voice")
 public class Controller {
 
-  private final VoiceService voiceService;
+  private final WebHooksService webhooks;
   private final ServerBusinessLogic webhooksBusinessLogic;
 
   @Autowired
   public Controller(VoiceService voiceService, ServerBusinessLogic webhooksBusinessLogic) {
-    this.voiceService = voiceService;
+    this.webhooks = voiceService.webhooks();
     this.webhooksBusinessLogic = webhooksBusinessLogic;
   }
 
@@ -31,16 +34,30 @@ public class Controller {
   public ResponseEntity<String> VoiceEvent(
       @RequestHeader Map<String, String> headers, @RequestBody String body) {
 
+    // ensure valid authentication to handle request
+    var validAuth =
+        webhooks.validateAuthenticatedRequest(
+            // The HTTP verb this controller is managing
+            "POST",
+            // The URI this controller is managing
+            "/VoiceEvent",
+            // request headers
+            headers,
+            // request payload body
+            body);
+
+    // token validation failed
+    if (!validAuth) {
+      throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+    }
+
     // decode the request payload
-    var event = voiceService.webhooks().unserializeWebhooksEvent(body);
+    var event = webhooks.unserializeWebhooksEvent(body);
 
     // let business layer process the request
     if (Objects.requireNonNull(event) instanceof CallEvent e) {
       return ResponseEntity.ok()
-          .body(
-              voiceService
-                  .webhooks()
-                  .serializeWebhooksResponse(webhooksBusinessLogic.processCallEvent(e)));
+          .body(webhooks.serializeWebhooksResponse(webhooksBusinessLogic.processCallEvent(e)));
     } else {
       throw new IllegalStateException("Unexpected value: " + event);
     }
