d6e3945ee9c7fdf4d930275486422203b70fdf1c
Jean-Pierre Portier <jean-pierre.portier@mailgun.com>
2024-07-31 11:22:47 +0200

    ci: Add Voice getting started compilation

--- getting-started/compile.sh
+++ getting-started/compile.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-(cd verification/user-verification-using-sms-pin/client && mvn clean package) || exit 1
-(cd sms/respond-to-incoming-message/server && mvn clean package) || exit 1
+DIRECTORIES="
+sms/respond-to-incoming-message/server
+verification/user-verification-using-sms-pin/client
+voice/respond-to-incoming-call/server
+"
+
+for DIRECTORY in $DIRECTORIES
+do
+ (cd "$DIRECTORY" && mvn clean package) || exit 1
+done
