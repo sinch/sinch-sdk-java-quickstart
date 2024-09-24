75f97b108c239c5e33ef48db2d64e34dccd35a3f
Jean-Pierre Portier <jean-pierre.portier@mailgun.com>
2024-09-24 10:26:34 +0200

    refactor (Voice): Remove unused event processing related to 'respond to incoming call' getting started

--- getting-started/voice/respond-to-incoming-call/server/src/main/java/com/mycompany/app/voice/ServerBusinessLogic.java
+++ getting-started/voice/respond-to-incoming-call/server/src/main/java/com/mycompany/app/voice/ServerBusinessLogic.java
@@ -3,11 +3,8 @@ package com.mycompany.app.voice;
 import com.sinch.sdk.domains.voice.models.svaml.ActionHangUp;
 import com.sinch.sdk.domains.voice.models.svaml.InstructionSay;
 import com.sinch.sdk.domains.voice.models.svaml.SVAMLControl;
-import com.sinch.sdk.domains.voice.models.webhooks.AnsweredCallEvent;
 import com.sinch.sdk.domains.voice.models.webhooks.DisconnectCallEvent;
 import com.sinch.sdk.domains.voice.models.webhooks.IncomingCallEvent;
-import com.sinch.sdk.domains.voice.models.webhooks.NotifyEvent;
-import com.sinch.sdk.domains.voice.models.webhooks.PromptInputEvent;
 import java.util.Collections;
 import java.util.logging.Logger;
 import org.springframework.stereotype.Component;
@@ -31,24 +28,7 @@ public class ServerBusinessLogic {
         .build();
   }
 
-  public SVAMLControl answered(AnsweredCallEvent event) {
-
-    LOGGER.info("Handle event: " + event);
-
-    return SVAMLControl.builder().build();
-  }
-
   public void disconnect(DisconnectCallEvent event) {
-
-    LOGGER.info("Handle event: " + event);
-  }
-
-  public void prompt(PromptInputEvent event) {
-
-    LOGGER.info("Handle event: " + event);
-  }
-
-  public void notify(NotifyEvent event) {
 
     LOGGER.info("Handle event: " + event);
   }
